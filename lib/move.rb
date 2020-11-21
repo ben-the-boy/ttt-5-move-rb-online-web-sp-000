77a4d1fce7338062a7815fbd7d0895c066b083d4
Ben Hayes <b.t.hayes@comcast.net>
2020-11-21 19:09:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,6 @@ end
 
 # code your input_to_index and move method here!
 
-input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def input_to_index(input)
   number = (input.to_i - 1)
   return number
